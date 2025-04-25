4057c0e8a853296f066bfab5e09efaa0c229658d
Raj-wdd <milindraj55@gmail.com>
2025-04-24 18:18:49 -0600

    completed the prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please input your first name: ");
+        string firstname = Console.ReadLine();
+        Console.Write("Please inout your last name is: ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
