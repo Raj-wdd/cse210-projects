327704209263a7f39de31398238e6c78978f7c9b
Raj-wdd <milindraj55@gmail.com>
2025-04-24 18:07:10 -0600

    Completed the 1st prep

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello C# World!");
+        // Console.WriteLine("Hello C# World!");
+        Console.Write("Please input your first name: ");
+        string firstname = Console.ReadLine();
+        Console.Write("Please inout your last name is: ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 } // this is one of the comments....//
+
