ebad1d8648344fc5dd8046d500e6969dd64ff751
Raj-wdd <milindraj55@gmail.com>
2025-04-29 09:34:34 -0600

    made a for loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,11 +5,20 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello C# World!");
-        Console.Write("Please input your first name: ");
-        string firstname = Console.ReadLine();
-        Console.Write("Please inout your last name is: ");
-        string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
-    }
+        // Console.Write("Please input your first name: ");
+        // string firstname = Console.ReadLine();
+        // Console.Write("Please inout your last name is: ");
+        // string lastname = Console.ReadLine();
+        // Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+        int x = 10;
+        if (x == 10)
+        {
+            Console.WriteLine("X is 10");
+        
+        }
+        for (int i = 0; i < x; i++)
+        {
+            Console.WriteLine($"Bob is cool: {i}");
+        }
+    }   
 } // this is one of the comments....//
-
