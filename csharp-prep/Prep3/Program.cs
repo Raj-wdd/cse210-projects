6ed447199cc847c2165a0207ea6bd4f274f8c9ee
Raj-wdd <milindraj55@gmail.com>
2025-04-29 10:00:05 -0600

    started prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        
     }
 }
