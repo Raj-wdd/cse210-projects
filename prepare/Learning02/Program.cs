9666677780aa53dd1e55fdb265d0308348c8f262
Raj-wdd <milindraj55@gmail.com>
2025-05-08 12:09:57 -0600

    Completed the learning acativity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Backend Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2017;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Google";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Raj Patil";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
